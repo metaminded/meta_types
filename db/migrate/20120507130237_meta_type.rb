0fc27f026f915151b85c79d0972d7526b5718699
Peter Horn <hornp@provideal.net>
2012-05-10 18:10:11 +0200

    docs

--- db/migrate/20120507130237_meta_type.rb
+++ db/migrate/20120507130237_meta_type.rb
@@ -1,9 +1,11 @@
 class MetaType < ActiveRecord::Migration
   def up
+    # we have a type column to allow STI to have a ProductType, a CustomerType and the like
     execute <<-CREATE_SQL
       create table meta_types (
         id          serial PRIMARY KEY,
         sid         character varying not null UNIQUE,
+        type        character varying,
         title       character varying not null UNIQUE,
         created_at  timestamp without time zone,
         updated_at  timestamp without time zone
